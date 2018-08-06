89c2d0f12028ce398d52f83aca59b6a9fe2c265e
duli <592306832@qq.com>
2018-08-07 00:13:56 +0800

    DelimiterBasedFrameDecoder

--- netty/netty-server/src/main/java/echo_demo/client/EchoClientHandler.java
+++ netty/netty-server/src/main/java/echo_demo/client/EchoClientHandler.java
@@ -8,14 +8,26 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.CharsetUtil;
 @ChannelHandler.Sharable
 public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    private int counter;
+    static final String ECHO_REQ = "Hi,DuLi.Welcome to Netty.$_";
+
     /**
      * 在服务器的连接已经建立之后将被调用
      */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        //当被通知Channel是活跃的时候，发送一条消息
-        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!",
-                CharsetUtil.UTF_8));
+        for(int i = 0;i< 10;i++){
+            ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }
+
+
+
+
+//        -------------------------------分隔符---------------------------------
+//        //当被通知Channel是活跃的时候，发送一条消息
+//        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!",
+//                CharsetUtil.UTF_8));
     }
 
     /**
@@ -26,6 +38,18 @@ public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
         System.out.println(
                 "Client received: " + byteBuf.toString(CharsetUtil.UTF_8)
         );
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("This is " + ++counter + " times receive server : ["
+                        + msg + "]"
+        );
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
     }
 
     /**
